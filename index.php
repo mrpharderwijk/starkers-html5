a016d0d22a11087f438a25bb8de576154c75fdad
Nathan Staines <nathan@nathanstaines.com>
2011-03-09 18:35:40 +0000

    version 3.1 updates

--- index.php
+++ index.php
@@ -6,12 +6,10 @@
  * @subpackage Starkers
  * @since Starkers HTML5 3.0
  */
-
+ 
 get_header(); ?>
-
-			<?php
-			 get_template_part( 'loop', 'index' );
-			?>
-
+ 
+    <?php get_template_part( 'loop', 'index' ); ?>
+ 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
